6cc0cf0f80374d73a48ac22b68c23a5e50104c96
alexandercbooth <alexander.c.booth@gmail.com>
2017-01-24 10:54:57 -0500

    test: change test for pycodestyle

--- nblint/tests/pyflakes.py
+++
@@ -1,5 +0,0 @@
-Code Cell 12 that starts with 'plt.scatte' has the following flake8                  errors
- tmp.py:6:1: invalid syntax
-%matplotlib inline
-^
-
--- nblint/tests/test_linters.py
+++ nblint/tests/test_linters.py
@@ -1,24 +1,24 @@
 import subprocess
 
-out = subprocess.Popen(["nblint", "../examplenbs/kmeans.ipynb"],
+out = subprocess.Popen(["nblint", "../examplenbs/py.ipynb"],
                        stdout=subprocess.PIPE,
                        stderr=subprocess.STDOUT
                        ).communicate()[0]
 
-out2 = subprocess.Popen(["nblint", "../examplenbs/kmeans.ipynb",
+out2 = subprocess.Popen(["nblint", "../examplenbs/py.ipynb",
                         "--linter", "pyflakes"],
                         stdout=subprocess.PIPE,
                         stderr=subprocess.STDOUT
                         ).communicate()[0]
 
-with open("pep8.py", "r") as f:
+with open("pycodestyle.txt", "r") as f:
     ff = f.read()
 
-with open("pyflakes.py", "r") as f2:
+with open("pyflakes.txt", "r") as f2:
     ff2 = f2.read()
 
 
-def test_pep8():
+def test_pycodestyle():
     assert ff == out.decode("utf8")
 
 
